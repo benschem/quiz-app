6826a605b25f89c1c0c87b8072565ffe6c8aec0b
Ben Schembri <benschembri@gmail.com>
2025-02-15 18:35:13 +1100

    update user and associated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :quizzes, through: :user_quiz, dependent: :destroy
+  has_many :user_quizzes, dependent: :destroy
+  has_many :quizzes, through: :user_quizzes, dependent: :destroy
+  has_many :guesses, dependent: :destroy
 
   validates :session_id, presence: true
 end
--- app/models/user_answer.rb
+++
@@ -1,5 +0,0 @@
-class Guess < ApplicationRecord
-  belongs_to :answer
-  validates :answer, presence: true
-  validates :session_id, presence: true
-end
